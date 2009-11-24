18756013505515c7927b703457f9b5e62488247a
Saokim999 <Saokim999@1b59e274-d830-11de-a65f-03a608dd9bca>
2009-11-24 16:52:16 +0000

    Việt hóa qua file config.inc.php cho dễ hiểu

--- config.inc.php
+++ config.inc.php
@@ -9,62 +9,62 @@
 	$Date: 2007/01/26 01:11:32 $
 */
 
-// [CH] 以下变量请根据空间商提供的账号参数修改,如有疑问,请联系服务器提供商
+// [CH] Duoi day la cac muc dien tham so, tai khoan CSDL. Neu co cau hoi nao moi ban vao http://traitimyenbai.net
 
-	$dbhost = 'localhost';			// 数据库服务器
-	$dbuser = 'username';			// 数据库用户名
-	$dbpw = '';				// 数据库密码
-	$dbname = 'discuz';			// 数据库名
-	$pconnect = 0;				// 数据库持久连接 0=关闭, 1=打开
+	$dbhost = 'localhost';			// Server Nam : Thuong dat la localhost
+	$dbuser = 'username';			// Database User Name
+	$dbpw = '';				// Database Password
+	$dbname = 'discuz';			// Database Name
+	$pconnect = 0;				// Persistent database connection 0 = closed, 1 = Open
 
-// [CH] Mysql 辅助服务器设置，只有当您拥有多个 Mysql 服务器且协同工作时请进行设置
+// [CH] Mysql Secondary server settings, only when you have more than one Mysql Server when you work together and set
 
-	$multiserver = array();			// 服务器变量初始化，请勿修改或删除
+	$multiserver = array();			// Server variable initialization, do not modify or delete
 	
-// [CH] 如您对 cookie 作用范围有特殊要求, 或论坛登录不正常, 请修改下面变量, 否则请保持默认
+// [CH] Bien doi de phu hoi cac nhu cau dang nhap dien dan. Ban co the de mac dinh
 
-	$cookiepre = 'cdb_';			// cookie 前缀
-	$cookiedomain = ''; 			// cookie 作用域
-	$cookiepath = '/';			// cookie 作用路径
+	$cookiepre = 'cdb_';			// cookie perfix
+	$cookiedomain = ''; 			// cookie Scope
+	$cookiepath = '/';			// cookie The role of path
 
-// [CH] 论坛投入使用后不能修改的变量
+// [CH] Forum in use can not be changed after the variable
 
-	$tablepre = 'cdb_';   			// 表名前缀, 同一数据库安装多个论坛请修改此处
+	$tablepre = 'cdb_';   			// Table Perfix. Dung de thay doi ten table. tranh bi trung khi cai nhieu ma nguon
 
-// [CH] 小心修改以下变量, 否则可能导致论坛无法正常使用
+// [CH] Can than thay doi cac tuy bien sau vi se anh huong den dien dan
 
-	$database = 'mysql';			// 论坛数据库类型，请勿修改
-	$dbcharset = '';			// MySQL 字符集, 可选 'gbk', 'big5', 'utf8', 'latin1', 留空为按照论坛字符集设定
+	$database = 'mysql';			// Loai CSDL. Tuyet doi khong thay doi
+	$dbcharset = '';			// MySQL ky tu dat ko bat buoc 'gbk', 'big5', 'utf8', 'latin1', De trong tu dong cai dat ma phu hop
 
-	$charset = 'utf-8';			// 论坛页面默认字符集, 可选 'gbk', 'big5', 'utf-8'
-	$headercharset = 0;			// 强制论坛页面使用默认字符集，可避免部分服务器空间页面出现乱码，一般无需开启。 0=关闭 1= 开启
+	$charset = 'utf-8';			// Dat ky tu mac dinh cho dien dan, tuy chon 'gbk', 'big5', 'utf-8'
+	$headercharset = 0;			// Mandatory forum pages use the default character set to avoid some server space page appears garbled, the general need to open. 0 = closed 1 = open
 
-	$tplrefresh = 1;			// 论坛风格模板自动刷新开关 0=关闭, 1=打开。
+	$tplrefresh = 1;			// Skin dien dan tu dong lam moi. De 1 la tu dong. 0 la tat.
 
-// [CH] 论坛安全设置, 调整以下设置，可以增强论坛的安全性能和防御性能
+// [CH] Cai dat bao mat cho dien dan traitimyenbai.net
 
-	$adminemail = 'admin@your.com';		// 系统管理员 Email
+	$adminemail = 'admin@your.com';		//  Email cua Admin
 
-	$forumfounders = '';			// 论坛创始人 UID, 可以支持多个创始人，之间使用 “,” 分隔。[出于安全考虑，请务必设置一名管理员为创始人]
-						// 论坛创始人可对其他管理员进行设置。如果不设置论坛创始人，则论坛管理员之间权利平等。
+	$forumfounders = '';			// ID của Root Admin, co the dung dau "," de phan cach giua cac id
+						// Neu ban dien id cua admin khac, thi coi nhu dong sang lap va co quyen ngang nhau.
 
-	$dbreport = 0;				// 论坛出现数据库错误时，是否通过 email 发送错误报告给系统管理员
+	$dbreport = 0;				// Khi co loi database, he thong se gui thong bao toi email Admin. Dat 0 = tat, 1 = Mo
 
-	$errorreport = 1;			// 是否屏蔽程序错误信息, 0=屏蔽所有错误(安全) 1=报告给管理员和版主(安全) 2=报告给任何人
+	$errorreport = 1;			// Bao loi: 0 = mask all errors (Safety), 1 cho admin va mod, 2 bao cho tat ca
 
-	$attackevasive = 0;			// 论坛防御级别，可防止大量的非正常请求造成的拒绝服务攻击
-						// 防护大量非正常请求造成的拒绝服务攻击,
-						// 0=关闭, 1=cookie 刷新限制, 2=限制代理访问, 4=二次请求, 8=回答问题（第一次访问时需要回答问题）
-						// 组合为: 1|2, 1|4, 2|8, 1|2|4...
+	$attackevasive = 0;			// Cau hinh de co the chan so luong lon yeu cau tu choi tan cong tu choi dich vu
+						// So luong yeu cau de han che tan cong
+						// 0=dong, 1=cookie= thiet lap gioi han, 2=Quy dinh han che truy cap, 4=Yeu cau thu 2, 8=Lan truy cap dau tien can tra loi cau hoi
+						// Co the ket hop nhu sau: 1|2, 1|4, 2|8, 1|2|4...
 	
-	$urlxssdefend = 1;			// 论坛访问页面防御开关，可避免用户通过非法的url地址对本站用户造成危害，建议打开。1=打开 0关闭
+	$urlxssdefend = 1;			// Forum defensive switch access page, users can avoid the url address of the illegal users of the site detrimental, it is recommended to open. 1 = open 0 closed
 
 	$admincp = array();
-	$admincp['forcesecques'] = 0;		// 管理人员必须设置安全提问才能进入系统设置, 0=否, 1=是[安全]
-	$admincp['checkip'] = 1;		// 后台管理操作是否验证管理员的 IP, 1=是[安全], 0=否。仅在管理员无法登陆后台时设置 0。
-	$admincp['tpledit'] = 0;		// 是否允许在线编辑论坛模板 1=是 0=否[安全]
-	$admincp['runquery'] = 1;		// 是否允许后台运行 SQL 语句 1=是 0=否[安全]
-	$admincp['dbimport'] = 1;		// 是否允许后台恢复论坛数据  1=是 0=否[安全]
+	$admincp['forcesecques'] = 0;		// Yeu cau bat buoc Admin phai cai dat cau hoi bao mat moi vao dc AdminCp 0= tat 1 = mo
+	$admincp['checkip'] = 1;		// Cho phep admin co the xac minh ip an toan ko. 
+	$admincp['tpledit'] = 0;		// Cho phep sua truc tuyen cac file. Khong an toan. Nen dat =0
+	$admincp['runquery'] = 0;		// Cho phep admin chay Query SQL trong Admincp  1=co 0=tat ( an toan)
+	$admincp['dbimport'] = 0;		// Cho phep admin Import Data. 1= mo, 0= at ( an toan)
 
 // ============================================================================
 
