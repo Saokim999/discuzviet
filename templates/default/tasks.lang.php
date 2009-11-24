949982a999b42352f49f2fef1d74b9d2dcf378bc
Saokim999 <Saokim999@1b59e274-d830-11de-a65f-03a608dd9bca>
2009-11-24 16:40:27 +0000

    Update tasks.lang.php

--- templates/default/tasks.lang.php
+++ templates/default/tasks.lang.php
@@ -1,23 +1,23 @@
-<?php
+﻿<?php
 
 $tasklang = array
 (
-	'avatar_name' => '头像类任务',
-	'avatar_desc' => '该任务仅限没有上传头像的会员申请，申请后只要上传头像即可完成任务，获得相应的奖励。',
-	'avatar_apply_var_name_noavatar' => '没有上传头像',
-	'avatar_apply_var_desc_noavatar' => '该任务仅限没有上传过头像的会员申请',
-	'avatar_complete_var_name_uploadavatar' => '上传头像',
-	'avatar_complete_var_desc_uploadavatar' => '申请任务后只要上传头像即可完成任务',
+	'avatar_name' => 'Tên nhiệm vụ',
+	'avatar_desc' => 'Vai trò không phải là để tải lên hình ảnh để áp dụng cho các thành viên, miễn là ứng dụng thành công của tải lên để hoàn thành sứ mệnh đầu, để có được những ưu đãi thích hợp.',
+	'avatar_apply_var_name_noavatar' => 'Trưởng không tải lên',
+	'avatar_apply_var_desc_noavatar' => 'Nhiệm vụ này chỉ dành cho thành viên chưa upload Avatar lần nào. Nếu bạn có rồi thì nhịn vậy^^',
+	'avatar_complete_var_name_uploadavatar' => 'Tải lên hình ảnh',
+	'avatar_complete_var_desc_uploadavatar' => 'Sau thành công của các sứ mệnh như lâu như các ứng dụng cho tải lên để hoàn thành nhiệm vụ đầu',
 	'avatar_copyright' => '<a href="http://www.comsenz.com" target="_blank">Comsenz Inc.</a>',
 
-	'gift_name' => '红包类任务',
-	'gift_desc' => '申请此任务即可领取红包。',
+	'gift_name' => 'Nhiệm vụ nhận thưởng',
+	'gift_desc' => 'Nhiệm vụ này có thể áp dụng cho các gói quà màu đỏ',
 	'gift_copyright' => '<a href="http://www.comsenz.com" target="_blank">Comsenz Inc.</a>',
 
-	'promotion_name' => '论坛推广任务',
-	'promotion_desc' => '您知道论坛推广在哪里吗？哈哈，在“<a href="memcp.php?action=credits" target="_blank">我的积分</a>”里面，页面下方有推广链接，复制后发给您的好友，或者发到您的博客，或者发到一些大论坛，推广获积分，还可以得到该任务的额外奖励。',
-	'promotion_complete_var_name_iplimit' => '推广 IP 数下限',
-	'promotion_complete_var_desc_iplimit' => '当日推广 IP 数大于或等于此设置，才能完成任务。',
+	'promotion_name' => 'Diễn đàn để thúc đẩy sứ mệnh',
+	'promotion_desc' => 'Bạn có biết nơi này diễn đàn để thúc đẩy？<a href="memcp.php?action=credits" target="_blank">Của tôi</a>Trong đó, có ở dưới cùng của trang để thúc đẩy sự liên kết, sau khi các bản sao gửi đến cho bạn bè của bạn, hoặc được thực hiện vào blog của bạn, hoặc thực hiện một số diễn đàn lớn để thúc đẩy hội nhập cũng có thể nhận được bổ sung nhiệm vụ của các giải thưởng.',
+	'promotion_complete_var_name_iplimit' => 'Khuyến mãi số lượng tối thiểu của IP ',
+	'promotion_complete_var_desc_iplimit' => 'Các chương trình khuyến mãi trong ngày, Số lượng là lớn hơn hoặc bằng thiết lập này, để hoàn thành nhiệm vụ.IP',
 	'promotion_copyright' => '<a href="http://www.comsenz.com" target="_blank">Comsenz Inc.</a>',
 
 );
