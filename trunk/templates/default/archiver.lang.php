dceb62cab4cd25e28a31a03cbe69fc852a1edd0f
Saokim999 <Saokim999@1b59e274-d830-11de-a65f-03a608dd9bca>
2009-11-24 16:35:20 +0000

    Lang Chinese sang Việt Nam

--- trunk/templates/default/archiver.lang.php
+++ trunk/templates/default/archiver.lang.php
@@ -1,18 +1,18 @@
 <?php
 
 // Message Pack for Discuz! Archiver Version 1.0.0
-// Created by Crossday
+// Created by Crossday Translate by Saokim999 - Discuz Viet Group
 
 $lang = array
 (
 
-	'page' => '页',
-	'replies' => '篇回复',
-	'anonymous' => '匿名',
-	'full_version' => '查看完整版本',
-	'forum_nonexistence' => '您没有权限访问这个论坛的存档或该论坛不存在。',
-	'thread_nonexistence' => '您没有权限查看这个话题或该话题不存在。',
-	'post_time' => '发表于',
+	'page' => 'Trang',
+	'replies' => 'Trả lời',
+	'anonymous' => 'Khách',
+	'full_version' => 'Xem bản đầy đủ',
+	'forum_nonexistence' => 'Bạn không có quyền truy cập diễn đàn hoặc Lưu trữ không tồn tại',
+	'thread_nonexistence' => 'Bạn không có quyền xem Bài này hoặc Bài không tồn tại',
+	'post_time' => 'vào lúc',
 
 );
 
