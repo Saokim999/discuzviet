2cfc351da8a443ab48a89719a546a85a0f5f1436
khanh.ct <khanh.ct@1b59e274-d830-11de-a65f-03a608dd9bca>
2009-11-26 08:59:43 +0000

    feed.lang.php translate completed

--- templates/default/feed.lang.php
+++ templates/default/feed.lang.php
@@ -3,37 +3,37 @@
 $language = array
 (
 
-	'feed_thread_title' =>			'{actor} 在 {app} 发表了新话题',
+	'feed_thread_title' =>			'{actor} viết bài mới tại {app}',
 	'feed_thread_message' =>		'{subject}<br />{message}',
 
-	'feed_reply_title' =>			'{actor} 在 {app} 回复了 {author} 的话题 {subject}',
+	'feed_reply_title' =>			'{actor} trả lời bài {app} bởi {author} đề tài {subject}',
 	'feed_reply_message' =>			'',
 
-	'feed_thread_poll_title' =>		'{actor} 在 {app} 发起了新投票',
+	'feed_thread_poll_title' =>		'{actor} tạo bình chọn mới tại {app}',
 	'feed_thread_poll_message' =>		'{subject}<br />{message}',
 
-	'feed_thread_votepoll_title' =>		'{actor} 在 {app} 参与了关于 {subject} 的投票',
+	'feed_thread_votepoll_title' =>		'{actor} tham gia bình chọn tại {app} về {subject}',
 	'feed_thread_votepoll_message' =>	'',
 
-	'feed_thread_goods_title' =>		'{actor} 在 {app} 出售了一个新商品',
-	'feed_thread_goods_message' =>		'{itemname}<br />售价 {itemprice} 元',
+	'feed_thread_goods_title' =>		'{actor} bán hàng tại {app}',
+	'feed_thread_goods_message' =>		'{itemname}<br />Giá {itemprice} đồng',
 
-	'feed_thread_reward_title' =>		'{actor} 在 {app} 发起了新悬赏',
-	'feed_thread_reward_message' =>		'{subject}<br />悬赏 {rewardprice} {extcredits}',
+	'feed_thread_reward_title' =>		'{actor} thưởng cho bài viết tại {app}',
+	'feed_thread_reward_message' =>		'{subject}<br />Giải thưởng: {rewardprice} {extcredits}',
 
-	'feed_reply_reward_title' =>		'{actor} 在 {app} 回复了关于 {subject} 的悬赏',
+	'feed_reply_reward_title' =>		'{actor} thưởng cho bài trả lời về {subject} tại {app}',
 	'feed_reply_reward_message' =>		'',
 
-	'feed_thread_activity_title' =>		'{actor} 在 {app} 发起了新活动',
-	'feed_thread_activity_message' =>	'开始时间：{starttimefrom}<br />活动地点：{activityplace}<br />每人大约 {cost} 元<br />{subject}<br />{message}',
+	'feed_thread_activity_title' =>		'{actor} đăng hoạt động mới tại {app}',
+	'feed_thread_activity_message' =>	'Bắt đầu lúc: {starttimefrom}<br />Tại nơi: {activityplace}<br />Giá: {cost} đồng<br />{subject}<br />{message}',
 
-	'feed_reply_activity_title' =>		'{actor} 在 {app} 报名参加了 {subject} 的活动',
+	'feed_reply_activity_title' =>		'{actor} tham gia hoạt động {subject} tại {app}',
 	'feed_reply_activity_message' =>	'',
 
-	'feed_thread_debate_title' =>		'{actor} 在 {app} 发起了新辩论',
-	'feed_thread_debate_message' =>		'正方：{affirmpoint}<br />反方：{negapoint}<br />{subject}<br />{message}',
+	'feed_thread_debate_title' =>		'{actor} đưa ra cuộc tranh luận mới tại {app}',
+	'feed_thread_debate_message' =>		'Đồng ý {affirmpoint}<br />Bác bỏ {negapoint}<br />{subject}<br />{message}',
 
-	'feed_thread_debatevote_title' =>	'{actor} 在 {app} 参与了关于 {subject} 的辩论',
+	'feed_thread_debatevote_title' =>	'{actor} tham gia tranh luận tại {app} về {subject}',
 	'feed_thread_debatevote_message' =>	'',
 
 );
