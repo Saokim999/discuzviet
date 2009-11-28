11a0f4b640e1812d058c7b7118d9b74c7ec868dc
khanh.ct <khanh.ct@1b59e274-d830-11de-a65f-03a608dd9bca>
2009-11-28 15:03:32 +0000

    Fixed some text ucenter admin.lang.php, main.lang.php

--- uc_server/view/default/admin.lang.php
+++ uc_server/view/default/admin.lang.php
@@ -3,11 +3,11 @@
 $lang = array(
 	'user_search' => 'Tìm',
 	'user_name' => 'Tên',
-	'user_regdate' => 'Thời gian đăng ký',
+	'user_regdate' => 'Ngày đăng ký',
 	'user_regip' => 'IP',
 	'user_before' => 'Trước',
 	'user_after' => 'Sau',
-	'user_search' => 'Submit',
+	'user_search' => 'Tìm',
 	'user_add' => 'Thêm thành viên',
 	'user_password' => 'Mật khẩu',
 	'user_addsubmit' => 'Submit',
